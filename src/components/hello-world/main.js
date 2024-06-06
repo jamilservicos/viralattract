7deb812f4a41b0a3e03e4cea9a13b71f6394326a
Gervasio Junior <jamilservicos@gmail.com>
2024-06-06 04:17:07 -0300

    Update main.js

--- src/components/hello-world/main.js
+++ src/components/hello-world/main.js
@@ -1,18 +1,11 @@
 'use strict';
 
-const mainText = {
-    "element": "h2",
-    "text": "Hello, World!"
-};
-
-const helloWordExample = (CoreModule) => {
-  return [
-      mainText,
-      {
-          "element": "h4",
-          "text": "by SimplyBuilder "+ CoreModule.version
-      }
-  ]
-};
+const helloWordExample = [
+    mainText,
+    {
+        element: "h4",
+        text: "come soon...."
+    }
+];
 
 export default Object.freeze(helloWordExample);
